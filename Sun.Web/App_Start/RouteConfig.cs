2e5098d4093f087c4151bd8070471b296cdc0b74
luohuazhiyu <553782463@qq.com>
2015-05-22 16:29:36 +0800

    set default router
    set default router

--- Sun.Web/App_Start/RouteConfig.cs
+++ Sun.Web/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace Sun.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "adminhome", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "adminhome", action = "Index", id = UrlParameter.Optional, areaname = "admin" }
             );
         }
     }
